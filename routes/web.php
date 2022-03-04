d5658af0a9981efd9271037aa39c677b7de367cb
elmarputz <github@elp.name>
2022-03-04 10:50:02 +0100

    routing basics!

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = "Fritz";
+    /* return view('welcome', [
+        "name" => $name
+    ]); */
+    return view('welcome', compact('name'));
 });
