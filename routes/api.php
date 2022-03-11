58ba483ff9eb2208df0fe0a5c5e293cc06ec5997
elmarputz <github@elp.name>
2022-03-11 15:06:21 +0100

    first API methods

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\JsonResponse;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('books',[BookController::class,'index']);
+Route::get('books/{isbn}', [BookController::class,'findByISBN']);
