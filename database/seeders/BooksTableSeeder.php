c99c8cc515f889321929df12dcf155a828a39d76
Elmar Putz <github@elp.name>
2022-03-11 11:30:47 +0100

    Update BooksTableSeeder.php

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -11,21 +11,27 @@ use DateTime;
 
 class BooksTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $book = new Book;
-        $book->title = "Herr der Ringe";
-        $book->isbn = Str::random(20);
-        $book->subtitle = Str::random(100);
-        $book->rating = 4;
-        $book->description = 'Letzer Teil der Reihe';
-        $book->published = new DateTime();
-        $book->save();
+  public function run(){
+   $book = new \App\Models\Book;
+   $book->title = "Herr der Ringe";
+   $book->isbn = "1234567890";
+   $book->subtitle = "Die Rückkehr des Königs";
+   $book->rating = 10;
+   $book->description = "Letzter Teil der Trilogie";
+   $book->published = new DateTime();
+   $book->save();
 
-    }
+   // add images to book
+   $image1 = new \App\Models\Image;
+   $image1->title = "Cover 1";
+   $image1->url = "https://images-na.ssl-images-amazon.com/images/I/61h%2BnIJyVFL._SX333_BO1,204,203,200_.jpg";
+
+   $image2 = new \App\Models\Image;
+   $image2->title = "Cover 2";
+   $image2->url = "https://images-eu.ssl-images-amazon.com/images/I/516KV5tjulL._AC_US327_FMwebp_QL65_.jpg";
+   $book->images()->saveMany([$image1,$image2]);
+   $book->save();
+  }
 }
+
+
